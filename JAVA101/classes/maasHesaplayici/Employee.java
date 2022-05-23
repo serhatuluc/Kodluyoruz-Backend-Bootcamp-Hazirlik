ed5e396f2c4b27cdd2d30a1fc510b6c34ebd1223
SerhatUluc <ulucs15@itu.edu.tr>
2022-05-24 00:06:33 +0300

    Sixth commit

---
+++ JAVA101/classes/maasHesaplayici/Employee.java
@@ -0,0 +1,63 @@
+package JAVA101.classes.maasHesaplayici;
+
+public class Employee {
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
+
+    public Employee(String name, double salary, int workHours,int hireYear){
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+    public double tax(){
+        if(this.salary < 1000){
+            return 0;
+        }
+        if(salary > 1000){
+            double tax = 0.03 * this.salary;
+            return tax;
+        }
+        return 0;
+    }
+
+    public double bonus(){
+        if(this.workHours > 40){
+            double bonus = (this.workHours - 40) * 30;
+            return bonus;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public double raiseSalary(){
+        double raiseRate;
+        int year = 2021 - this.hireYear;
+
+        if(year < 10){
+            raiseRate = 0.05;
+        } else if (year > 9 && year < 20) {
+            raiseRate = 0.1;
+        } else {
+            raiseRate = 0.15;
+        }
+
+        return this.salary*raiseRate;
+
+    }
+
+    public void toStr(){
+        System.out.println("Adı : " + this.name);
+        System.out.println("Maaşı : " + this.salary);
+        System.out.println("Çalışma Saati : " + this.workHours);
+        System.out.println("Başlangıç Yılı : " + this.hireYear);
+        System.out.println("Vergi : " + this.tax());
+        System.out.println("Bonus : " + this.bonus());
+        System.out.println("Maaş Artışı : " + this.raiseSalary());
+        System.out.println("Vergi ve Bonuslar ile birlikte maaş : " + (this.salary+ this.bonus() - this.tax()));
+        System.out.println("Toplam Maaş : " + (this.salary + this.raiseSalary()));
+    }
+}
---
+++ JAVA101/classes/maasHesaplayici/Main.java
@@ -0,0 +1,8 @@
+package JAVA101.classes.maasHesaplayici;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee worker = new Employee("kemal",2000, 45, 1985);
+        worker.toStr();
+    }
+}
