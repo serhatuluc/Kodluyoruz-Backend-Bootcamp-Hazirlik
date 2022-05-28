59071d10bb96126d95694cc0dda1d9079e2e6f6a
SerhatUluc <ulucs15@itu.edu.tr>
2022-05-28 23:21:00 +0300

    Fifteenth commit

---
+++ JAVA101/others/palindromikKelime.java
@@ -0,0 +1,19 @@
+package JAVA101.others;
+
+public class palindromikKelime {
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("abba"));
+    }
+}
